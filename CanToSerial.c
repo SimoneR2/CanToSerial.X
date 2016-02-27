a12dabcc9b0981e7bcd80b5f113fe53d5180c1c0
Simone <simone.righetti97@gmail.com>
2016-02-27 19:08:13 +0100

    New version

--- CanToSerial.c
+++ CanToSerial.c
@@ -17,17 +17,20 @@
 =   RB2/RB3 => CANBus                =            
 ======================================
  */
-#include <string.h>
-#include <stdio.h>
+//#include <string.h>
+//#include <stdio.h>
 #define _XTAL_FREQ 16000000
 #include <xc.h>
 #include "pic18_config.h"
 #include "idCan.h"
 #include "CANlib.h"
-#include <usart.h>
+#include "usart.h"
 #include "delay.h"
 #include "delay.c"
+
 void configurazione_iniziale(void);
+void can_interpreter(void);
+
 volatile bit newMessageCan = 0;
 volatile bit newMessageUsart = 0;
 CANmessage msg;
@@ -39,10 +42,12 @@ unsigned char data [8] = 0;
 
 volatile bit dir = 0;
 volatile unsigned char set_steering = 0;
-volatile unsigned int set_speed = 0;
-volatile unsigned char set_speed_pk1 = 0;
-volatile unsigned char set_speed_pk0 = 0;
+volatile unsigned char set_speed[8] = 0;
 volatile unsigned char analogic_brake = 0;
+
+volatile unsigned char set_steering_old = 0;
+volatile unsigned char set_speed_old[8] = 0;
+volatile unsigned char analogic_brake_old = 0;
 
 __interrupt(low_priority) void ISR_bassa(void) {
     if ((PIR3bits.RXB0IF == 1) || (PIR3bits.RXB1IF == 1)) {
@@ -55,40 +60,56 @@ __interrupt(low_priority) void ISR_bassa(void) {
         PIR3bits.RXB0IF = 0;
         PIR3bits.RXB1IF = 0;
     }
-    if (PIR1bits.RCIF == 1) {
+    if (PIR1bits.RC1IF == 1) {
         getsUSART((char*) USART_Rx, 7);
         if ((USART_Rx[0] == 0xAA)&&(USART_Rx[6] == 0xAA)) {
-            dir = USART_Rx[1];
-            set_speed_pk1 = USART_Rx[2];
-            set_speed_pk0 = USART_Rx[3];
+
+            set_steering_old = set_steering;
+            set_speed_old[0] = set_speed[0];
+            set_speed_old[1] = set_speed[1];
+            analogic_brake_old = analogic_brake;
+
+            set_speed[2] = USART_Rx[1]; //direction
+            set_speed[0] = USART_Rx[2];
+            set_speed[1] = USART_Rx[3];
             set_steering = USART_Rx[4];
             analogic_brake = USART_Rx[5];
-            set_speed = (set_speed_pk1 << 8)||(set_speed_pk0);
         }
+        PIR1bits.RC1IF = 0;
+        ;
     }
 }
 
 void main(void) {
+    configurazione_iniziale();
     while (1) {
         if (newMessageCan == 1) {
-            putsUSART((char *) usartTx);
+            can_interpreter();
+            putsUSART((char *) USART_Tx);
             newMessageCan = 0;
         }
         if (newMessageUsart == 1) {
-            RTR = usartRx [0];
-            id = usartRx[2];
-            id = ((id << 8) | usartRx[1]);
-            for (int i = 0; i < 8; i++) {
-                data[i] = usartRx[(i + 3)];
+            if (set_steering_old != set_steering) {
+                CANsendMessage(STEERING_CHANGE, set_steering, 8, CAN_CONFIG_STD_MSG & CAN_NORMAL_TX_FRAME & CAN_TX_PRIORITY_0);
             }
-            if (CANisTxReady()) {
-                if (RTR == 1) {
-                    CANsendMessage(id, data, 8, CAN_CONFIG_STD_MSG & CAN_REMOTE_TX_FRAME & CAN_TX_PRIORITY_0);
-                } else {
-                    CANsendMessage(id, data, 8, CAN_CONFIG_STD_MSG & CAN_NORMAL_TX_FRAME & CAN_TX_PRIORITY_0);
-                }
+            if ((set_speed_old[0] != set_speed[0])&&(set_speed_old[1] != set_speed[1])) {
+                CANsendMessage(SPEED_CHANGE, set_speed, 8, CAN_CONFIG_STD_MSG & CAN_NORMAL_TX_FRAME & CAN_TX_PRIORITY_0);
             }
+            if (analogic_brake_old != analogic_brake) {
+               CANsendMessage(BRAKE_SIGNAL, set_speed, 8, CAN_CONFIG_STD_MSG & CAN_NORMAL_TX_FRAME & CAN_TX_PRIORITY_0);
+            }
+             newMessageUsart = 0;
         }
+    }
+}
+
+void can_interpreter(void) {
+    if (id == ECU_STATE) {
+        //(if RTR_Flag == 1){
+        CANsendMessage(id, data, 8, CAN_CONFIG_STD_MSG & CAN_NORMAL_TX_FRAME & CAN_TX_PRIORITY_0);
+        // Motore_Flag = 1;
+    } else {
+
     }
 }
 
@@ -98,15 +119,15 @@ void configurazione_iniziale(void) {
     RCONbits.IPEN = 1; //abilita priorità interrupt
     PIR3bits.RXB1IF = 0; //azzera flag interrupt can bus buffer1
     PIR3bits.RXB0IF = 0; //azzera flag interrupt can bus buffer0
-    PIR1bits.RCIF = 0; //azzera flag interrupt ricezione seriale
+    PIR1bits.RC1IF = 0; //azzera flag interrupt ricezione seriale
 
     IPR3bits.RXB1IP = 0; //interrupt bassa priorità per can
     IPR3bits.RXB0IP = 0; //interrupt bassa priorità per can
-    IPR1bits.RCIP = 0; //interrupt bassa priorità ricezione seriale
+    IPR1bits.RC1IP = 0; //interrupt bassa priorità ricezione seriale
 
     PIE3bits.RXB1IE = 1; //abilita interrupt ricezione can bus buffer1
     PIE3bits.RXB0IE = 1; //abilita interrupt ricezione can bus buffer0
-    PIE1bits.RCIE = 1; //abilita interrupt ricezione seriale
+    PIE1bits.RC1IE = 1; //abilita interrupt ricezione seriale
 
     INTCONbits.GIEH = 1; //abilita interrupt 
     INTCONbits.GIEL = 1; //abilita interrupt periferiche
@@ -119,7 +140,7 @@ void configurazione_iniziale(void) {
     OpenUSART(USART_TX_INT_OFF & USART_RX_INT_ON & USART_ASYNCH_MODE & USART_EIGHT_BIT
             & USART_BRGH_HIGH & USART_CONT_RX, 103); //Seriale asincrona, 8bit, 9600baud
 
-    RCSTAbits.SPEN = 1; //abilita la periferica
+    //RCSTAbits.SPEN = 1; //abilita la periferica
 
     LATA = 0x00;
     TRISA = 0b01111101;
@@ -130,10 +151,4 @@ void configurazione_iniziale(void) {
     LATC = 0x00;
     TRISC = 0b10111111;
 
-    LATD = 0x00;
-    TRISD = 0x00;
-
-    LATE = 0x00;
-    TRISE = 0xFF;
-    delay_ms(2);
 }
